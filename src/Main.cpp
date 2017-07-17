0d8d866984e09298b6c382564d606a351d11482a
Jacob Garby <j.garby@icloud.com>
2017-07-17 03:49:51 +0100

    more docs

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,27 +17,44 @@ WINDOW * stat_win;
 
 void curses_init()
 {
+    // init ncurses screen, making main window too
     initscr();
+    // change how terminal input works
+    // initialise colours
     start_color();
+    // don't echo typed characters back
     noecho();
+    // characters read 1 by 1
     cbreak();
+    // get all keys
     keypad(stdscr, true);
 
+    // make colours pairs
+    // they can be reference like
+    // COLOR_PAIR(index)
+    // the first parameter of each init
+    // is the index
     init_pair(1, COLOR_BLACK, COLOR_WHITE); // black on white
     init_pair(2, COLOR_WHITE, COLOR_BLUE); // insert
     init_pair(3, COLOR_BLUE, COLOR_WHITE); // normal
 
+    // The window for the title of jedit, at the top
     title_win = newwin(1, COLS, 0, 0);
+    // set colour of window - white bg
     wbkgd(title_win, COLOR_PAIR(1));
+    // add a title
     wprintw(title_win, "jedit\t");
-    wclrtoeol(title_win);
+    // display title
     wrefresh(title_win);
 
+    // init the window for the actual text editing
     editor_win = newwin(LINES - 2, COLS, 1, 0);
+    // the window for the status, at the bottom e.g. filename, mode
     stat_win = newwin(1, COLS, LINES-1, 0);
 
-    redrawwin(editor_win);
-    redrawwin(title_win);
+    // redraw both windows
+    //redrawwin(editor_win);
+    //redrawwin(title_win);
 
     wbkgd(stat_win, COLOR_PAIR(1));
 
