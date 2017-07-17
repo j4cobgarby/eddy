f9845028e8bfd103f7b716e0567e6587e2238829
Jacob Garby <j.garby@icloud.com>
2017-07-17 01:04:00 +0100

    documented some stuff

--- src/Widgets.cpp
+++ src/Widgets.cpp
@@ -2,10 +2,10 @@
 
 void showDialog(string title, vector<string> body, int w) {
     string input;
-    int h = 6 + body.size();
+    int height = 6 + body.size();
 
-    WINDOW * dia = newwin(h, w, (LINES / 2) - (h / 2), (COLS / 2) - (w / 2));
-    mvwprintw(dia, h - 2, 2, "[ENTER] to continue");
+    WINDOW * dia = newwin(height, w, (LINES / 2) - (height / 2), (COLS / 2) - (w / 2));
+    mvwprintw(dia, height - 2, 2, "[ENTER] to continue");
     wborder(dia, 0, 0, 0, 0, 0, 0, 0, 0);
 
     mvwprintw(dia, 1, (w - title.length()) / 2, title.c_str());
@@ -25,19 +25,35 @@ void showDialog(string title, vector<string> body, int w) {
 }
 
 string getDialogInput(string title, vector<string> body, int w) {
-    int max = w - 6;
-    int h = 9 + body.size();
+    /*
+     * max: max characters in input
+     * input_width: visual width of input field
+     * height: height of dialog window
+     * scroll_amount: amount scrolled in input field, horizontally
+     * input: the typed text for the input field
+     */
+    int max = 128;
+    int input_width = w - 6;
+    int height = 9 + body.size();
+    int scroll_amount = 0;
+
     string input;
 
-    WINDOW * dia = newwin(h, w, (LINES / 2) - (h / 2), (COLS / 2) - (w / 2));
-    mvwprintw(dia, h-2, 2, "[ENTER] to submit");
+    // Center the window
+    WINDOW * dia = newwin(height, w, (LINES / 2) - (height / 2), (COLS / 2) - (w / 2));
+    mvwprintw(dia, height-2, 2, "[ENTER] to submit");
+    // 0 is the default border
     wborder(dia, 0, 0, 0, 0, 0, 0, 0, 0);
 
+    // print title string
     mvwprintw(dia, 1, 2, title.c_str());
+
+    // iterate body vector and print each line in it
     int i = 0;
     for (auto const& ln: body) { i++;
         mvwprintw(dia, 3 + i, 3, ln.c_str());
     }
+    // initial display of field
     wattron(dia, A_REVERSE);
     mvwprintw(dia, 5 + body.size(), 3, string(max, ' ').c_str());
     wattroff(dia, A_REVERSE);
@@ -45,16 +61,31 @@ string getDialogInput(string title, vector<string> body, int w) {
     wrefresh(dia);
     redrawwin(dia);
     int d_c;
+    // Until enter is pressed, constantly update value of d_c by getting the
+    // character pressed
     while (((d_c = getch()) != KEY_ENTER) && d_c != 10) {
+        // If backspace is pressed, and backspacing won't go past the beginning
+        // of the input
         if ((d_c == KEY_BACKSPACE || d_c == 127) && input.length() > 0) {
             input.pop_back();
-        } else if ((d_c >= 32 && d_c <= 126) && input.length() + 1 <= max) {
+        }
+        // If the pressed character is an allowed ascii character, and the
+        // length limit won't be exceeded
+        else if ((d_c >= 32 && d_c <= 126) && input.length() + 1 <= max) {
             input += d_c;
         }
+        // Black on white
         wattron(dia, A_REVERSE);
-        mvwprintw(dia, 5 + body.size(), 3, string(max, ' ').c_str());
+
+        // Draw input box
+        mvwprintw(dia, 5 + body.size(), 3, string(input_width, ' ').c_str());
+
+        // Draw text buffer
         mvwprintw(dia, 5 + body.size(), 3, input.c_str());
+
+        // White on black
         wattroff(dia, A_REVERSE);
+
         wrefresh(dia);
         redrawwin(dia);
     }
