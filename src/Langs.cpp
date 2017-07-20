10192b06c8bab01843f277a2eb60af9e8cbd2494
Jacob Garby <j.garby@icloud.com>
2017-07-20 19:48:35 +0100

    fixed syntax error

--- src/Langs.cpp
+++ src/Langs.cpp
@@ -5,8 +5,8 @@ void Editor::initLangs() {
     {"number", R"(([+-]?(\d*[.])?\d+))"},
     {"operator", R"((\+|-|\*|\/|%|\*\*|\/\/|==|!=|>=|<=|<<|>>|>|<|=|\+=|-=|\*=|\/=|%=|\*\*=|\/\/=|&|\||\^|~|@))"},
     {"keyword", "(continue|nonlocal|finally|assert|except|global|import|lambda|return|False|break|class|raise|while|yield|None|True|elif|else|from|pass|with|and|def|del|for|not|try|as|if|in|is|or)"},
-    {"function", R"((\w+)\()"}
-    {"string", R"((\"((\\\"|[^\"\n])*)\"))"},
+    {"function", R"((\w+)\()"},
+    {"string", R"((\"((\\\"|[^\"\n])*)\"))"}
   };
   this->langs["python3"] = python3;
   file_extension_to_lang["python3"] = vector<string>{
