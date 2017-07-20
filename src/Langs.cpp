1444a9c597679e04719ff8dadfc06152d14b67d9
Jacob Garby <j.garby@icloud.com>
2017-07-20 17:03:23 +0100

    made number regex use cap. groups

--- src/Langs.cpp
+++ src/Langs.cpp
@@ -2,7 +2,7 @@
 
 void Editor::initLangs() {
   map<string, string> python3 {
-    {"number", R"([+-]?(\d*[.])?\d+)"},
+    {"number", R"(([+-]?(\d*[.])?\d+))"},
     {"string", R"(\"((\\\"|[^\"\n])*)\")"},
     {"keyword", "False|None|True|and|as|asset|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|in|import|in|is|is not|lambda|nonlocal|not|not in|or|pass|raise|return|try|while|with|yield"},
     {"operators", R"(\+|-|\*|\/|%|\*\*|\/\/|==|!=|>=|<=|<<|>>|>|<|=|\+=|-=|\*=|\/=|%=|\*\*=|\/\/=|&|\||\^|~|@)"}
